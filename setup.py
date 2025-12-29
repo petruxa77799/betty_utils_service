11226dabde44c9252d455377a4f8c0f8cb19f64e
Alexey Dergunov <dergunovalexey2000@gmail.com>
2025-12-29 13:17:25 +0700

    upd

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name="betty_utils_service",
     description="betty utils service for auth and errors",
-    version="0.2.12",
+    version="0.2.13",
     license="MIT",
     author="Petr Shcherbakov-Sandu",
     author_email="petrscherbakov93@gmail.com",
@@ -11,5 +11,5 @@ setup(
     package_dir={"": "src"},
     url="https://github.com/petruxa77799/betty_utils_service",
     keywords="betty utils service",
-    install_requires=["pyjwt==2.10.1", "ujson==5.10.0", "aiokafka==0.12.0"],
+    install_requires=["pyjwt>=2.10.1", "ujson>=5.10.0", "aiokafka>=0.12.0"],
 )
